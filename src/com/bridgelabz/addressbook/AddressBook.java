a1fb42f727f53eed6aa840a91f1ecec661a835b3
mudable <mudableshilpa@gmail.com>
2021-11-07 19:41:23 +0530

    Added : UC2 to add the new contacts

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -1,7 +1,67 @@
 package com.bridgelabz.addressbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+	static ArrayList<Contact> contact = new ArrayList<>();
+
+	public static void addContact() {
+		System.out.println("Enter the number of contacts ");
+		Scanner sc = new Scanner(System.in);
+		int numberOfContacts = sc.nextInt();
+		for (int i = 0; i < numberOfContacts; i++) {
+			// System.out.println("Enter the details of contact number " + (i + 1));
+			System.out.println("Enter  first name");
+			String firstName = sc.next();
+			System.out.println("Last name");
+			String lastName = sc.next();
+			System.out.println("Enter address");
+			String address = sc.next();
+			System.out.println("Enter city ");
+			String city = sc.next();
+			System.out.println("Enter state ");
+			String state = sc.next();
+			System.out.println("Enter EmailId ");
+			String email = sc.next();
+			System.out.println("Enter zip ");
+			int zip = sc.nextInt();
+			System.out.println("Enter phone number ");
+			int phoneNum = sc.nextInt();
+			
+Contact details = new Contact(firstName, lastName, address, city, state, phoneNum, zip, email);
+			contact.add(details);
+		}
+	}
+
+	public static void showContacts() {
+		int i = 1;
+		for (Contact contact_info : contact) {
+			// System.out.println("Details of contact number " + i + " is");
+			System.out.println(contact.toString());
+			i++;
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Wel Come to Address Book program.");
+
+		System.out.println("Welcome to address book programming");
+
+		int i = 1;
+		while (i != 0) {
+			System.out.println("Enter 1 to add contact or  Enter 2 to display details.");
+			Scanner sc = new Scanner(System.in);
+			int userChoice = sc.nextInt();
+			switch (userChoice) {
+			case 1:
+				addContact();
+				break;
+			case 2:
+				showContacts();
+				break;
+			default:
+				System.out.println("Invalid Input");
+			}
+		}
 	}
 }
--- src/com/bridgelabz/addressbook/Contact.java
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -1,5 +1,26 @@
 package com.bridgelabz.addressbook;
 
 public class Contact {
+	String firstName, lastName, address, city, state;
+	long phoneNum, zip;
+	String email;
 
+	public Contact(String firstName, String lastName, String address, String city, String state, long phoneNum,
+			long zip, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.phoneNum = phoneNum;
+		this.zip = zip;
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "[firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", phoneNum=" + phoneNum + ", zip=" + zip + ", email=" + email + "]";
+	}
 }
